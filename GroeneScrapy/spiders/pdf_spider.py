cbfeb4af706ec769fb61b127b6ff1ef474e579ad
Pieter <@>
2017-01-30 22:41:00 +0100

    DOM of Groene website changed (on 1 Jan 2017?)

--- GroeneScrapy/spiders/pdf_spider.py
+++ GroeneScrapy/spiders/pdf_spider.py
@@ -7,33 +7,37 @@ from GroeneScrapy.items import GroeneNummer
 class PdfSpider(scrapy.Spider):
     name = "pdfspider"
     allowed_domains = ["www.groene.nl"]
-    start_urls = ["https://www.groene.nl/home"]
+    start_urls = ["https://www.groene.nl/accounts/inloggen"]
     
     def parse(self, response):
         return scrapy.FormRequest.from_response(
             response,
-            url="https://www.groene.nl/gebruiker/inloggen",
+            formnumber = 1,
             method="post",
             formdata={
-                      'user[email]': settings.GROENE_USERNAME, 
-                      'user[password]': settings.GROENE_PASSWORD
+                      'customer[email]': settings.GROENE_USERNAME,
+                      'customer[password]': settings.GROENE_PASSWORD
                      },
             callback=self.after_login
         )
 
     def after_login(self, response):
-        if "E-mailadres en/of wachtwoord is onjuist." in response.body:
+        if "Ongeldig e-mailadres of wachtwoord." in response.body:
             print "ERROR: Login failed"
             return
-        for sel in response.xpath("/html/body/div[@id='loggedin-tooltip']/ul/li[a='Download pdf']/a/@href"):
+        editionPageRequest = scrapy.Request("https://www.groene.nl/nieuwste-editie", callback = self.edition_page)
+        yield editionPageRequest
+
+    def edition_page(self, response):
+        for sel in response.xpath("//a[.='Download pdf']/@href"):
             item = GroeneNummer()
-            relUrl = sel.extract()
-            item['jaar'] = relUrl.split('/')[1]
-            item['weeknummer'] = relUrl.split('/')[2].split('.')[0]
-            pdfRequest = scrapy.Request("https://www.groene.nl" + relUrl, callback = self.download_pdf)
+            relUrl = response.url
+            item['jaar'] = relUrl.split('/')[-2]
+            item['weeknummer']  = relUrl.split('/')[-1]
+            pdfRequest = scrapy.Request("https://www.groene.nl" + sel.extract(), callback = self.download_pdf)
             pdfRequest.meta['item'] = item
             yield pdfRequest
-    
+
     def download_pdf(self, response):
         item = response.meta['item']
         with open(os.path.join(settings.GROENE_PDF_PATH, item['jaar'] + "-" + item['weeknummer'].zfill(2) + ".pdf"), 'w') as f:
